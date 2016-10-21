de089900b38e3c163d739763530a377bc98d18f8
Christopher Perez <cfperez97@gmail.com>
2016-10-21 17:00:03 -0500

    Project Created and Added Files
    The Eclipse Project was created and some files were added.

---
+++ src/Infrastructure.java
@@ -0,0 +1,5 @@
+public class Infrastructure {
+	// includes ramps, highway, and exits
+	// must figure out how to represent distance
+	
+}
---
+++ src/SimulationRunner.java
@@ -0,0 +1,6 @@
+public class SimulationRunner {
+	public static void main(String[] args) {
+		// runs simulation ideally with a "real time" graphic
+		
+	}
+}
